48bf2ca45c848a5fdbcd6d91a0c90a52ac140af1
quickversedev <qvdev@quickverse.in>
2025-07-11 23:20:23 +0530

    Header (#15)
    * added alot of thing,header, profile,navigation , navigation screens
    
    * added header , navigationtabs
    
    * add scroll animation
    
    * removed unnecessory files
    
    * added the home background image
    
    * made compatile across all the devices
    
    ---------
    
    Co-authored-by: Abhilashghope <abhilashghope111@gmail.com>

--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Animated, Image, StyleSheet, View } from 'react-native';
+import { Animated, Image, Platform, SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
 import { NavigationItems } from '../../../screens/Home/components/NavigationItems';
 import { useTheme } from '../../../theme/ThemeContext';
 import { LocationSelector } from './LocationSelector';
@@ -22,41 +22,43 @@ export const Header: React.FC<HeaderProps> = ({ translateY, hiddenSectionsOpacit
         translateY ? { transform: [{ translateY }] } : undefined,
       ]}
     >
-      {/* Sections that will be hidden on scroll */}
-      <Animated.View
-        style={[
-          styles.hiddenSections,
-          hiddenSectionsOpacity ? { opacity: hiddenSectionsOpacity } : undefined,
-        ]}
-      >
-        {/* Section 1: Top Row */}
-        <View style={styles.topRow}>
-          <LocationSelector />
-          <ProfileIcon />
-        </View>
+      <SafeAreaView style={styles.safeArea}>
+        {/* Sections that will be hidden on scroll */}
+        <Animated.View
+          style={[
+            styles.hiddenSections,
+            hiddenSectionsOpacity ? { opacity: hiddenSectionsOpacity } : undefined,
+          ]}
+        >
+          {/* Section 1: Top Row */}
+          <View style={styles.topRow}>
+            <LocationSelector />
+            <ProfileIcon />
+          </View>
 
-        {/* Section 3: Logo */}
-        <View style={styles.logoContainer}>
-          <Image
-            source={require('../../../assets/images/logo_qv.png')}
-            style={styles.logo}
-            resizeMode="contain"
-          />
-        </View>
-      </Animated.View>
+          {/* Section 3: Logo */}
+          <View style={styles.logoContainer}>
+            <Image
+              source={require('../../../assets/images/homeBackground.png')}
+              style={styles.logo}
+              resizeMode="cover"
+            />
+          </View>
+        </Animated.View>
 
-      {/* Sections that will remain visible */}
-      <View style={styles.visibleSections}>
-        {/* Section 2: Search Bar */}
-        <View style={styles.searchSection}>
-          <SearchBar />
-        </View>
+        {/* Sections that will remain visible */}
+        <View style={styles.visibleSections}>
+          {/* Section 2: Search Bar */}
+          <View style={styles.searchSection}>
+            <SearchBar />
+          </View>
 
-        {/* Section 4: Navigation Tabs */}
-        <View style={[styles.tabContainer, { borderBottomColor: theme.colors.border }]}>
-          <NavigationItems />
+          {/* Section 4: Navigation Tabs */}
+          <View style={[styles.tabContainer, { borderBottomColor: theme.colors.border }]}>
+            <NavigationItems />
+          </View>
         </View>
-      </View>
+      </SafeAreaView>
     </Animated.View>
   );
 };
@@ -65,14 +67,24 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     position: 'absolute',
-    top: 20,
+    top: 0,
     left: 0,
     right: 0,
     zIndex: 1000,
+    ...Platform.select({
+      android: {
+        paddingTop: StatusBar.currentHeight ? StatusBar.currentHeight - 15 : 0,
+      },
+      ios: {
+        paddingTop: 0,
+      },
+    }),
+  },
+  safeArea: {
+    width: '100%',
   },
   hiddenSections: {
     paddingHorizontal: 16,
-    paddingTop: 12, // Slightly increased top padding
   },
   visibleSections: {
     width: '100%',
@@ -81,20 +93,27 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginBottom: 16,
+    paddingTop: Platform.select({
+      ios: 2,
+      android: 0,
+    }),
   },
   searchSection: {
     width: '100%',
     paddingHorizontal: 16,
-    marginBottom: 16,
   },
   logoContainer: {
-    alignItems: 'center',
-    marginVertical: 20,
+    width: '100%',
+    height: Platform.select({
+      ios: 150,
+      android: 130,
+    }),
+    marginHorizontal: -16,
+    overflow: 'hidden',
   },
   logo: {
-    width: 200,
-    height: 40,
+    width: '100%',
+    height: '100%',
   },
   tabContainer: {
     borderBottomWidth: 3,
