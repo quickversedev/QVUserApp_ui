ca98fbd343975d88a387249cbf1aede4cd89fbc4
Abhilashghope <abhilashghope111@gmail.com>
2025-07-11 23:17:52 +0530

    made compatile across all the devices

--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Animated, Image, StyleSheet, View } from 'react-native';
+import { Animated, Image, Platform, SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
 import { NavigationItems } from '../../../screens/Home/components/NavigationItems';
 import { useTheme } from '../../../theme/ThemeContext';
 import { LocationSelector } from './LocationSelector';
@@ -22,41 +22,43 @@ export const Header: React.FC<HeaderProps> = ({ translateY, hiddenSectionsOpacit
         translateY ? { transform: [{ translateY }] } : undefined,
       ]}
     >
-      {/* Sections that will be hidden on scroll */}
-      <Animated.View
-        style={[
-          styles.hiddenSections,
-          hiddenSectionsOpacity ? { opacity: hiddenSectionsOpacity } : undefined,
-        ]}
-      >
-        {/* Section 1: Top Row */}
-        <View style={styles.topRow}>
-          <LocationSelector />
-          <ProfileIcon />
-        </View>
+      <SafeAreaView style={styles.safeArea}>
+        {/* Sections that will be hidden on scroll */}
+        <Animated.View
+          style={[
+            styles.hiddenSections,
+            hiddenSectionsOpacity ? { opacity: hiddenSectionsOpacity } : undefined,
+          ]}
+        >
+          {/* Section 1: Top Row */}
+          <View style={styles.topRow}>
+            <LocationSelector />
+            <ProfileIcon />
+          </View>
 
-        {/* Section 3: Logo */}
-        <View style={styles.logoContainer}>
-          <Image
-            source={require('../../../assets/images/homeBackground.png')}
-            style={styles.logo}
-            resizeMode="cover"
-          />
-        </View>
-      </Animated.View>
+          {/* Section 3: Logo */}
+          <View style={styles.logoContainer}>
+            <Image
+              source={require('../../../assets/images/homeBackground.png')}
+              style={styles.logo}
+              resizeMode="cover"
+            />
+          </View>
+        </Animated.View>
 
-      {/* Sections that will remain visible */}
-      <View style={styles.visibleSections}>
-        {/* Section 2: Search Bar */}
-        <View style={styles.searchSection}>
-          <SearchBar />
-        </View>
+        {/* Sections that will remain visible */}
+        <View style={styles.visibleSections}>
+          {/* Section 2: Search Bar */}
+          <View style={styles.searchSection}>
+            <SearchBar />
+          </View>
 
-        {/* Section 4: Navigation Tabs */}
-        <View style={[styles.tabContainer, { borderBottomColor: theme.colors.border }]}>
-          <NavigationItems />
+          {/* Section 4: Navigation Tabs */}
+          <View style={[styles.tabContainer, { borderBottomColor: theme.colors.border }]}>
+            <NavigationItems />
+          </View>
         </View>
-      </View>
+      </SafeAreaView>
     </Animated.View>
   );
 };
@@ -69,6 +71,17 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     zIndex: 1000,
+    ...Platform.select({
+      android: {
+        paddingTop: StatusBar.currentHeight ? StatusBar.currentHeight - 15 : 0,
+      },
+      ios: {
+        paddingTop: 0,
+      },
+    }),
+  },
+  safeArea: {
+    width: '100%',
   },
   hiddenSections: {
     paddingHorizontal: 16,
@@ -80,18 +93,22 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    // marginBottom: 5,
+    paddingTop: Platform.select({
+      ios: 2,
+      android: 0,
+    }),
   },
   searchSection: {
     width: '100%',
     paddingHorizontal: 16,
-    // marginBottom: 16,
   },
   logoContainer: {
     width: '100%',
-    height: 160,
-    // marginVertical: 5,
-    marginHorizontal: -16, // Compensate for parent padding
+    height: Platform.select({
+      ios: 150,
+      android: 130,
+    }),
+    marginHorizontal: -16,
     overflow: 'hidden',
   },
   logo: {
